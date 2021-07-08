3aa8076fe00a4066bd7672b15c4c3489d316a1ad
shurankain <shurankain@gmail.com>
2021-07-08 11:32:37 +0200

    initial commit

---
+++ src/main/java/com/shurankain/springsoapwsclient/SpringSoapWsClientApplication.java
@@ -0,0 +1,13 @@
+package com.shurankain.springsoapwsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoapWsClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSoapWsClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shurankain/springsoapwsclient/SpringSoapWsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shurankain.springsoapwsclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSoapWsClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
